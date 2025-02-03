8ba7a13048c3ad54bf138d89db58301d56d07309
BHUMIKA JOSHI <joshibhumika555@gmail.com>
2025-02-03 06:07:53 +0000

    datatype-summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,12 @@
+//primitive == call by value
+//->7 types
+//string , boolean , number , null, undefined , bigint , symbol
+
+//non-primitive == call by reference
+//  -> array,object,function 
+
+
+//
+// typeof
+// null=>object
+// undefined->undefined
