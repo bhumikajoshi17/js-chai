bd332a7ce08dece1bd28a254bc03e19e5fbb4d53
BHUMIKA JOSHI <joshibhumika555@gmail.com>
2025-02-03 05:38:10 +0000

    operation

---
+++ 01_basics/operations.js
@@ -0,0 +1,12 @@
+let value =3
+let negValue= -value
+//console.log(negValue);
+
+
+// console.log("1" +2);
+// console.log(1+ "2");
+// console.log("1"+ 2+2);
+// console.log(1+3+"3");
+
+console.log(+true);
+console.log(+"")
